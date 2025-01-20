05f2622be01bf373fa8d922879012b2de2e0cb04
Shawn Witte <shawn.l.witte@gmail.com>
2025-01-19 17:11:37 -0800

    Add non-compose Jvm application configuration (#12)
    * Add non-compose Jvm application configuration
    
    * Update version
    
    * Fix imports per detekt style recommendations

--- src/main/kotlin/plugin/JvmAppConventionPlugin.kt
+++ src/main/kotlin/plugin/JvmAppConventionPlugin.kt
@@ -3,6 +3,7 @@ package plugin
 import configuration.applyUniversalConfigurations
 import configuration.configureCompose
 import configuration.configureJvm
+import configuration.configureJvmApp
 import configuration.configureSerialization
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -11,7 +12,6 @@ public class JvmAppConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         with(target) {
             with(pluginManager) {
-                // apply("application") // TODO have this as a configuration if needed as alternative to composeApp
                 apply("org.jetbrains.kotlin.jvm")
             }
             applyUniversalConfigurations(useGradleChecker = false)
@@ -24,5 +24,6 @@ public class JvmAppConventionPlugin : Plugin<Project> {
 
 public open class MinirogueJvmAppExtension(private val project: Project) {
     public fun composeApp(mainClass: String): Unit = project.configureCompose(mainClass)
+    public fun jvmApp(mainClass: String): Unit = project.configureJvmApp(mainClass)
     public fun serialization(): Unit = project.configureSerialization()
 }
