d935e87a561ae32f5aa92e4a88f3820981acede9
shwitte <ettiwsl@gmail.com>
2025-02-21 21:32:10 -0800

    Update version in readme and Versions.kt

--- src/main/kotlin/ext/Versions.kt
+++ src/main/kotlin/ext/Versions.kt
@@ -1,6 +1,6 @@
 package ext
 
 // TODO would be good to represent these in a way that dependabot can pick up
-internal const val ROOM_VERSION = "2.7.0-alpha12"
+internal const val ROOM_VERSION = "2.7.0-beta01"
 internal const val SERIALIZATION_VERSION = "1.8.0"
 internal const val JAVA_LIB_VERSION = "17"
