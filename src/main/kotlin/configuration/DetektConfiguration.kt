2d8cb2fe2de0927a81380458b4a8f036ea9a7cec
Shawn Witte <shawn.l.witte@gmail.com>
2025-03-19 21:10:00 -0700

    Use one detekt config for whole project (#30)
    * swap ktlint with detekt formatting
    
    * Use a single detekt config for whole project

--- src/main/kotlin/configuration/DetektConfiguration.kt
+++ src/main/kotlin/configuration/DetektConfiguration.kt
@@ -12,14 +12,21 @@ private const val CREATE_DETEKT_CONFIG_TASK = "createDetektConfig"
 internal fun Project.configureDetekt() {
     plugins.apply(DetektPlugin::class.java)
     // Add formatting, which is a wrapper around ktlint
-    dependencies.add("detektPlugins", "io.gitlab.arturbosch.detekt:detekt-formatting:$DETEKT_VERSION")
+    dependencies.add(
+        "detektPlugins",
+        "io.gitlab.arturbosch.detekt:detekt-formatting:$DETEKT_VERSION"
+    )
 
-    tasks.register(CREATE_DETEKT_CONFIG_TASK, CreateDetektConfigTask::class.java)
-    tasks.withType(Detekt::class.java) { dependsOn(CREATE_DETEKT_CONFIG_TASK) }
+    if (rootProject.tasks.none { it.name == CREATE_DETEKT_CONFIG_TASK }) {
+        // Creates single instance of this task in root project, then all projects' detekt tasks can
+        //   depend on the single config file produced by this single task
+        rootProject.tasks.register(CREATE_DETEKT_CONFIG_TASK, CreateDetektConfigTask::class.java)
+    }
+    tasks.withType(Detekt::class.java) { dependsOn(rootProject.tasks.getByName(CREATE_DETEKT_CONFIG_TASK)) }
 
     extensions.configure(DetektExtension::class.java) {
         source.setFrom("src/main", "src/androidMain", "src/commonMain", "src/jvmMain")
-        config.setFrom(files(tasks.getByName(CREATE_DETEKT_CONFIG_TASK)))
+        config.setFrom(files(rootProject.tasks.getByName(CREATE_DETEKT_CONFIG_TASK)))
         buildUponDefaultConfig = true
         autoCorrect = true
     }
