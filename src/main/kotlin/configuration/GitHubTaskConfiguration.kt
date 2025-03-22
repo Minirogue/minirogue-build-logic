0e4e86d210c4b78cc417a57aa6eaa24141f3117d
Shawn Witte <shawn.l.witte@gmail.com>
2025-03-21 17:42:43 -0700

    add common github workflow and dependabot configurations (#33)
    * add common github workflow and dependabot configurations

---
+++ src/main/kotlin/configuration/GitHubTaskConfiguration.kt
@@ -0,0 +1,13 @@
+package configuration
+
+import org.gradle.api.Project
+import task.CreateGitHubConfigTask
+
+private const val  CREATE_GITHUB_CONFIG_TASK = "createGitHubConfig"
+
+internal fun Project.configureGitHubConfigTask() {
+    // Creates single instance of this task in root project
+    if (rootProject.tasks.none { it.name == CREATE_GITHUB_CONFIG_TASK }) {
+        rootProject.tasks.register(CREATE_GITHUB_CONFIG_TASK, CreateGitHubConfigTask::class.java)
+    }
+}
