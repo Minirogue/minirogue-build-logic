a7fe3072101f589bf5c8196097c1a0b60a8d7d7f
shwitte <ettiwsl@gmail.com>
2025-06-01 02:13:53 -0700

    add android unit test directory from createSrc task

--- src/main/kotlin/task/CreateSrcTask.kt
+++ src/main/kotlin/task/CreateSrcTask.kt
@@ -26,6 +26,7 @@ internal open class CreateSrcTask @Inject constructor(sourceType: SourceType) : 
         SourceType.CommonMultiplatform -> project.files(sourceDirectory("commonMain"), sourceDirectory("commonTest"))
         SourceType.AndroidMultiplatform -> project.files(
             sourceDirectory("androidMain"),
+            sourceDirectory("androidUnitTest"),
             sourceDirectory("androidInstrumentedTest"),
         )
         SourceType.JvmMultiplatform -> project.files(sourceDirectory("jvmMain"), sourceDirectory("jvmTest"))
